e7a15a1e644e448bbf2beae3fb2484e25d1ac617
Nithish kumar <nithishjipmer2020@gmail.com>
2023-09-07 21:46:36 +0530

    commit

--- JS/chicken_game.js
+++ JS/chicken_game.js
@@ -1,24 +1,22 @@
-const RIGHT_HEN_SRC = "../images/chicken_left.jpg";
-const LEFT_HEN_SRC = "../images/chicken_right.jpg";
+const RIGHT_HEN_SRC = "../images/chicken_left.png";
+const LEFT_HEN_SRC = "../images/chicken_right.png";
 const INVERSION_PROBABLITY = 0.25;
 const HEN_DISPLAY_TIME = 1000;
 const MINIMUM_DISPLAY_TIME = 400;
 const FEEDBACK_DISPLAY_TIME = 200;
+const TOTAL_TIME = 60;
 
-const images = document.querySelectorAll("img");
+const images = document.querySelectorAll(".chicken");
 const imageList = [LEFT_HEN_SRC, RIGHT_HEN_SRC];
 const feedbackContainer = document.getElementById("feedback-container");
-const timer = document.getElementById("timerDiv");
+const yesbtn = document.getElementsByClassName("answer-btn")[0];
+const nobtn = document.getElementsByClassName("answer-btn")[1];
 
 var hensNotSame = false;
 var score = 0;
 var lives = 3;
-let timerInterval; // Variable to store the timer interval
-let secondsLeft = 60;
-
 
 function tryInvertHen() {
-  roundNumber++;
   // Probablity to invert
   if (Math.random() < INVERSION_PROBABLITY) {
     if (Math.random() > 0.5) {
@@ -73,28 +71,17 @@ function invertOneHenCol() {
 }
 
 function startGame() {
-  timer.style.visibility = "visible";
+  yesbtn.style.display = "inline";
+  nobtn.style.display = "inline";
   // Start the timer
-  timerInterval = setInterval(function () {
-    secondsLeft--;
-    document.getElementById("timer").textContent = secondsLeft;
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval); // Stop the timer
-      gameOver(); // Call the game over function
-    }
-  }, 1000);
+  progress(60, TOTAL_TIME, document.getElementById("progressBar"));
 
   tryInvertHen();
   setTimeout(askQuestion, 2000);
 }
 
 function resetGame() {
-  score = 0;
-  lives = 3;
-  secondsLeft = 60;
-  timer.style.visibility = "visible";
-  tryInvertHen();
-  setTimeout(askQuestion, 2000);
+  location.reload();
 }
 
 function returnDisplayTime() {
@@ -105,10 +92,17 @@ function returnDisplayTime() {
   return MINIMUM_DISPLAY_TIME;
 }
 
+let timeoutId;
+
 function loopGame() {
   // hide question container
   tryInvertHen();
-  setTimeout(askQuestion, returnDisplayTime());
+  showImages(true);
+
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(askQuestion, 1000);
 }
 
 function askQuestion() {
@@ -125,7 +119,8 @@ function showImages(shouldShow) {
   }
 }
 function answer(response) {
-
+  yesbtn.style.display = "none";
+  nobtn.style.display = "none";
   if (!response == hensNotSame) {
     // answer correct
     score += 10;
@@ -141,6 +136,8 @@ function answer(response) {
 
 function updateScreen() {
   document.getElementById("score").innerText = score;
+  const heartIcons = document.querySelectorAll(".heart-icon");
+  // heartIcons[lives].classList = [];
 }
 
 function removeLife() {
@@ -163,14 +160,26 @@ function restartGame() {
 }
 
 function gameOver() {
-  // clear bg
-  timer.setAttribute("style", "visibility: hidden");
+  // hide bg
+  showImages(false);
+  yesbtn.style.display = "none";
+  nobtn.style.display = "none";
+  document.getElementById("progressBar").style.display = "none";
+  document.getElementById("msg").style.display = "none";
 
   // show gameover
   const gameOverDiv = document.getElementById("gameOverDiv");
   const scoreDisplay = document.getElementById("scoreDisplay");
+  const highscoreDisplay = document.getElementById("highscoreDisplay");
   scoreDisplay.textContent = score;
   gameOverDiv.style.display = "block";
+
+  // highscore
+  var highscore = getHighScore("highscore");
+  highscoreDisplay.textContent = highscore;
+  if (score > highscore) {
+    setHighScore("highscore", score);
+  }
 }
 
 function showFeedbackSymbol(response) {
@@ -189,26 +198,12 @@ function showFeedbackSymbol(response) {
     feedbackContainer.setAttribute("style", "display: none");
 
     if (lives > 0) {
-      showImages(true);
       loopGame();
+      yesbtn.style.display = "inline";
+      nobtn.style.display = "inline";
     }
   }, FEEDBACK_DISPLAY_TIME);
 }
-
-// function lock() {
-//   if (
-//     navigator.userAgent.match(/Android/i) ||
-//     navigator.userAgent.match(/iPhone/i) ||
-//     navigator.userAgent.match(/BlackBerry/i) ||
-//     navigator.userAgent.match(/Windows Phone/i)
-//   ) {
-//     let de = document.documentElement;
-//     if (de.requestFullscreen) {
-//       de.requestFullscreen();
-//     }
-//     screen.orientation.lock("landscape");
-//   }
-// }
 
 document
   .getElementById("startGameButton")
@@ -219,3 +214,33 @@ document
     // Call the function to start the game
     startGame();
   });
+
+// bar timer
+function progress(timeleft, timetotal, element) {
+  var progressBar = element.querySelector("div");
+  var progressBarWidth = (timeleft * element.offsetWidth) / timetotal;
+
+  progressBar.style.width = progressBarWidth + "px";
+  progressBar.innerHTML = Math.floor(timeleft / 60) + ":" + (timeleft % 60);
+
+  if (timeleft > 0) {
+    setTimeout(function () {
+      progress(timeleft - 1, timetotal, element);
+    }, 1000);
+  } else {
+    gameOver();
+  }
+}
+
+// high score logic
+function setHighScore(id, val) {
+  localStorage.setItem(id, val);
+}
+
+function getHighScore(id) {
+  const highscore = localStorage.getItem(id);
+  if (highscore == null) {
+    return 0;
+  }
+  return highscore;
+}
